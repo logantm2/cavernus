b2906292284ccb83adb2aee8c4dce6b7b5382a48
Logan Meredith <loganmeredit@gmail.com>
2023-07-04 19:55:38 -0600

    Add abstract class for initial condition

---
+++ InitialConditions.py
@@ -0,0 +1,15 @@
+import mfem.par as mfem
+import numpy as np
+import abc
+
+class InitialInelasticCreepStrain(mfem.VectorPyCoefficient, abc.ABC):
+    def __init__(self, dim):
+        mfem.VectoyPyCoefficient.__init__(self, dim)
+
+    @abc.abstractmethod
+    def EvalValue(self, x):
+        pass
+
+class ZeroInitialInelasticCreepStrain(InitialInelasticCreepStrain):
+    def EvalValue(self, x):
+        return np.zeros(self.vdim)
