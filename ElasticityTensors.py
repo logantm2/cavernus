dc422985b2a3ef2acf5fd234b03e52ba03c14d85
Logan Meredith <loganmeredit@gmail.com>
2023-07-13 00:10:35 -0600

    Elasticity tensor can calculate stress

--- ElasticityTensors.py
+++ ElasticityTensors.py
@@ -1,3 +1,5 @@
+import Utils
+import mfem.par as mfem
 import abc
 
 class ElasticityTensor(abc.ABC):
@@ -6,6 +8,20 @@ class ElasticityTensor(abc.ABC):
     @abc.abstractmethod
     def evaluate(self, x, i, j, k, l):
         pass
+
+    # TODO: optimize this. Can probably skip the unflatten->flatten steps
+    def calcFlattenedStress(self, x, flattened_elastic_strain):
+        elastic_strain = Utils.unflattenSymmetricTensor(flattened_elastic_strain)
+        dims = elastic_strain.Height()
+        stress = mfem.DenseMatrix(dims)
+        stress.Assign(0.0)
+        for i in range(dims):
+            for j in range(dims):
+                for k in range(dims):
+                    for l in range(dims):
+                        stress[i,j] += self.evaluate(x, i, j, k, l) * elastic_strain[k, l]
+
+        return Utils.flattenSymmetricTensor(stress)
 
 class ConstantIsotropicElasticityTensor(ElasticityTensor):
     def __init__(self, l, mu):
