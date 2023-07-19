dbf1b73511d373b7208675360418171784d67b87
Logan Meredith <loganmeredit@gmail.com>
2023-07-19 12:44:53 -0500

    Boundary conditions inherit from mfem VectorCoef

--- BoundaryConditions.py
+++ BoundaryConditions.py
@@ -1,11 +1,25 @@
-class BoundaryCondition():
+import mfem.par as mfem
+import numpy as np
+import abc
+
+class BoundaryCondition(mfem.VectorPyCoefficient, abc.ABC):
     def __init__(
         self,
+        vdim,
         boundary_attribute,
         type
     ):
+        mfem.VectorPyCoefficient.__init__(self, vdim)
         self.boundary_attribute = boundary_attribute
         self.type = type
 
-    def eval(self, x, t, values):
-        values.Assign(0.0)
+    @abc.abstractmethod
+    def EvalValue(self, x):
+        pass
+
+class ZeroBoundaryCondition(BoundaryCondition):
+    def __init__(self, boundary_attribute, type):
+        super().__init__(1, boundary_attribute, type)
+
+    def EvalValue(self, x):
+        return np.zeros(x.Size())
